b522d0593a181d5194b5c5e588fd5c07ec897b46
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2020-04-06 18:46:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,58 +1,71 @@
 import discord
 from discord.ext import commands
-import random
+import os
+import traceback
+
+bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-bot = commands.Bot(command_prefix='?')
-
 @bot.event
-async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
+async def on_message(message):
+    """メンバー募集 (.rect@数字)"""
+    if message.content.startswith(".rect"):
+        mcount = int(message.content[6:len(message.content)])
+        text= "あと{}人 参加待機中\n"
+        revmsg = text.format(mcount)
+        #friend_list 押した人のList
+        frelist = []
+        msg = await bot.send_message(message.channel, revmsg)
 
-@bot.command()
-async def add(ctx, left: int, right: int):
-    await ctx.send(left + right)
+        #投票の欄
+        await bot.add_reaction(msg, '\u21a9')
+        await bot.add_reaction(msg, '⏫')
+        await bot.pin_message(msg)
 
-@bot.command()
-async def roll(ctx, dice: str):
+        #リアクションをチェックする
+        while len(frelist) < int(message.content[6:len(message.content)]):
+            target_reaction = await bot.wait_for_reaction(message=msg)
+            #発言したユーザが同一でない場合 真
+            if target_reaction.user != msg.author:
+                #==============================================================
+                #押された絵文字が既存のものの場合 >> 左　del
+                if target_reaction.reaction.emoji == '\u21a9':
+                    #==========================================================
+                    #◀のリアクションに追加があったら反応 frelistにuser.nameがあった場合　真
+                    if target_reaction.user.name in frelist:
+                        frelist.remove(target_reaction.user.name)
+                        mcount += 1
+                        #リストから名前削除
+                        await bot.edit_message(msg, text.format(mcount) +
+                                                        '\n'.join(frelist))
+                            #メッセージを書き換え
 
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
+                    else:
+                        pass
+                #==============================================================
+                #押された絵文字が既存のものの場合　>> 右　add
+                elif target_reaction.reaction.emoji == '⏫':
+                    if target_reaction.user.name in frelist:
+                        pass
 
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
+                    else:
+                        frelist.append(target_reaction.user.name)
+                        #リストに名前追加
+                        mcount = mcount - 1
+                        await bot.edit_message(msg, text.format(mcount) +
+                                                        '\n'.join(frelist))
 
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(ctx, *choices: str):
 
-    await ctx.send(random.choice(choices))
+                elif target_reaction.reaction.emoji == '✖':
+                        await bot.edit_message(msg, '募集終了\n'+ '\n'.join(frelist))
+                        await bot.unpin_message(msg)
+                        break
+                await bot.remove_reaction(msg, target_reaction.reaction.emoji, target_reaction.user)
+                #ユーザーがつけたリアクションを消す※権限によってはエラー
+                #==============================================================
+        else:
+            await bot.edit_message(msg, '募集終了\n'+ '\n'.join(frelist))
 
-@bot.command()
-async def repeat(ctx, times: int, content='repeating...'):
 
-    for i in range(times):
-        await ctx.send(content)
 
-@bot.command()
-async def joined(ctx, member: discord.Member):
-
-    await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
-
-@bot.group()
-async def cool(ctx):
-
-    if ctx.invoked_subcommand is None:
-        await ctx.send('No, {0.subcommand_passed} is not cool'.format(ctx))
-
-@cool.command(name='bot')
-async def _bot(ctx):
-
-    await ctx.send('Yes, the bot is cool.')
-
-bot.run('token')
+bot.run(token)
